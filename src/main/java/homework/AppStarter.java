f94d89dc4dc4eddbf57051d830cac60e37eda8f3
Boris Braga <feofart@gmail.com>
2023-05-03 13:07:57 +0300

    Added AppStarter

---
+++ src/main/java/homework/AppStarter.java
@@ -0,0 +1,16 @@
+package homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+@SpringBootApplication
+public class AppStarter {
+        private static final Logger log = Logger.getLogger(AppStarter.class.getName());
+
+        public static void main(String[] args) {
+            log.info("Start main..");
+            SpringApplication.run(AppStarter.class,args);
+            log.info("Finish main..");
+        }
+    }
