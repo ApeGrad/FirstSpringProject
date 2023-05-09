d63408bbdccb768b35544342acf40d4af51b0908
Boris Braga <feofart@gmail.com>
2023-05-09 19:01:57 +0300

    Permission working

--- src/main/java/homework/AppStarter.java
+++ src/main/java/homework/AppStarter.java
@@ -9,8 +9,8 @@ public class AppStarter {
         private static final Logger log = Logger.getLogger(AppStarter.class.getName());
 
         public static void main(String[] args) {
-            log.info("Started my main..");
+            log.info("Started my main...");
             SpringApplication.run(AppStarter.class,args);
-            log.info("Finished my main..");
+            log.info("Finished my main...");
         }
     }
